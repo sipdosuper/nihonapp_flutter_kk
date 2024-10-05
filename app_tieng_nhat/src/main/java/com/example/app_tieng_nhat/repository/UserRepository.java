c77ab5bd457dda1785dfa1d46ff80fec5677b5fc
sipdosuper <tranvikhanh2003@gmail.com>
2024-10-05 21:30:10 +0700

    cm

--- app_tieng_nhat/src/main/java/com/example/app_tieng_nhat/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.app_tieng_nhat.repository;
-
-import com.example.app_tieng_nhat.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<Users,Long> {
-}
--- duan/android/app/src/main/kotlin/com/example/duan/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.duan
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
